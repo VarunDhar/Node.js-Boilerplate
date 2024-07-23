a47a56776c3461470d3fa0852205e20364ebc210
VarunDhar <85066986+VarunDhar@users.noreply.github.com>
2024-07-23 16:26:39 +0530

    Delete src/routes/v1/aeroplane-routes.js

--- src/routes/v1/aeroplane-routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {aeroplaneController} = require("../../controllers");
-
-router.post('/create',aeroplaneController.createAeroplane);
-
-module.exports = router;
